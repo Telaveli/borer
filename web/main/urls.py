076b350f3cf574a4ecdd9e0596fcff294071fd3f
Avtandil Pangani <avto@telaveli.ge>
2021-10-31 08:19:02 +0400

    სტატიკა

--- web/main/urls.py
+++ web/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('generic/', views.generic),
+    path('elements/', views.elements)
 ]
