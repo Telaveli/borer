27c7567fd7d31a4a471d04dc38adabbd25c48177
Avtandil Pangani <avto@telaveli.ge>
2021-11-01 09:10:03 +0400

    gitignore

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class g_menu(models.model):
+    menu_item = models.CharFild(max_lent)
+    aa = forms.BooleanField(aa, required=False)
+    FIELDNAME = forms.CharField(, max_length=, required=False)
+    
