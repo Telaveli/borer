dab37379ce5284f94b2518e1eb44e087bd67bce2
Avtandil Pangani <avto@telaveli.ge>
2021-11-02 20:48:14 +0400

    მიგრაცია ამოვიღე

--- web/main/models.py
+++ web/main/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 
 class smenu(models.Model):
-    
+    mname = models.CharField("სახელი", max_length=20)
+    level = models.IntegerField("დონე", default=0)
+    hkey = models.BigIntegerField("კოდი")
 
     class Meta:
-        verbose_name = ("მენიუ")
-        verbose_name_plural = ("მენიუ")
+        verbose_name = ("Menu")
+        verbose_name_plural = ("Memus")
 
     def __str__(self):
-        return self.name
+        return self.mname
 
-    def get_absolute_url(self):
-        return reverse("smenu_detail", kwargs={"pk": self.pk})
+
