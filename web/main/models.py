ca13f5a51b38ecf39fc50c2219df4767e68ac5cf
Avtandil Pangani <avto@telaveli.ge>
2021-11-02 20:27:41 +0400

    models

--- web/main/models.py
+++ web/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class smenu(models.Model):
+    
+
+    class Meta:
+        verbose_name = ("მენიუ")
+        verbose_name_plural = ("მენიუ")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("smenu_detail", kwargs={"pk": self.pk})
